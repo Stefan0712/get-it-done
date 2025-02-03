2c9d906aa6db1d883b982cd26a5e173ed12dc863
Stefan0712 <s.vladulescu@gmail.com>
2025-02-03 14:52:22 +0200

    Fixed some height problems when going in and out of fullscreen on my phone. Added an useEffect inside App.js that dinamically set the height of the .App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 import Pomodoro from './components/Pomodoro/Pomodoro';
 import SideMenu from './components/SideMenu/SideMenu';
 import Tasks from './components/Tasks/Tasks';
+import { useEffect } from 'react';
 
 function App() {
-
+  
+  useEffect(() => {
+    const setAppHeight = () => {
+      const height = window.innerHeight;
+      document.documentElement.style.setProperty('--app-height', `${height}px`);
+    };
+  
+    setAppHeight();
+    window.addEventListener('resize', setAppHeight);
+  
+    return () => {
+      window.removeEventListener('resize', setAppHeight);
+    };
+  }, []);
+  
   
 
 
