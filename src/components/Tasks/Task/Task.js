fe9c3e1bd55812071e297b77761a2109bcd40867
Stefan0712 <s.vladulescu@gmail.com>
2025-02-03 13:46:32 +0200

    Added the toggleTaskCompletion reducer to each task's checkbox

--- src/components/Tasks/Task/Task.js
+++ src/components/Tasks/Task/Task.js
@@ -1,10 +1,19 @@
+import { toggleTaskCompletion } from '../../../store/tasksSlice';
 import styles from '../Tasks.module.css';
+import { useDispatch } from 'react-redux';
 
 const Task = ({data}) => {
+
+    const dispatch = useDispatch();
+
+    const handleCheckTask = ()=>{
+        dispatch(toggleTaskCompletion(data.id))
+    }
+
     return ( 
         <div className={styles.task}>
             <h3>{data.title}</h3>
-            <input type='checkbox' checked={data.isCompleted}></input>
+            <input type='checkbox' onChange={handleCheckTask} checked={data.isCompleted}></input>
         </div>
      );
 }
