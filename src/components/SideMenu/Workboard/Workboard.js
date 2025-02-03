0833dd96215ea0d399b89697777ffef8e1637f71
Stefan0712 <s.vladulescu@gmail.com>
2025-02-03 14:57:21 +0200

    Replaced hardcoded projects with the ones from my mock data

--- src/components/SideMenu/Workboard/Workboard.js
+++ src/components/SideMenu/Workboard/Workboard.js
@@ -1,21 +1,20 @@
 import styles from './Workboard.module.css';
+import { useSelector } from 'react-redux';
 
 
 const Workboard = ({isExpanded}) => {
+
+    const projects = useSelector(state=>state.projects)
+
     return ( 
         <div className={`${styles.workboard}`}>
-            <div className={styles.item}>
-                {isExpanded ? <h4>Project 1</h4> : null}
-                <p>4</p>
-            </div>
-            <div className={styles.item}>
-                {isExpanded ? <h4>Project 2</h4> : null}
-                <p>12</p>
-            </div>
-            <div className={styles.item}>
-                {isExpanded ? <h4>Project 3</h4> : null}
-                <p>7</p>
-            </div>
+            {projects && projects.length > 0 ? projects.map((project,index)=>(
+                <div key={'project-'+index} className={styles.item} style={{backgroundColor: project.color}}>
+                    {isExpanded ? <h4>{project.title}</h4> : null}
+                    <p>{project.tasks.length}</p>
+                </div>
+            )): null}
+            
         </div>
      );
 }
