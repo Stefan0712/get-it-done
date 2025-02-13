9705c0d4e900cbd4c587f13b8c6de394e6779130
Stefan0712 <s.vladulescu@gmail.com>
2025-02-13 14:24:43 +0200

    Changed the logic to get due tasks instead of pinned tasks and removed unused functions and imports

--- src/components/MainPanel/Home/Home.js
+++ src/components/MainPanel/Home/Home.js
@@ -1,6 +1,5 @@
 import styles from './Home.module.css';
 import { useState, useEffect } from 'react';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { useSelector } from 'react-redux';
 import Habbit from './Habbit';
@@ -8,10 +7,7 @@ import Habbit from './Habbit';
 
 const Home = () => {
 
-    const projects = useSelector((state)=>state.projects);
     const tasks = useSelector((state)=>state.tasks.tasks);
-    const pinnedTasks = useSelector((state)=>state.tasks.tasks.filter(item=>item.isPinned))
-
     const [currentTime, setCurrentTime] = useState(new Date());
     const date = new Date();
     const todayDate = date.toISOString().split("T")[0];
@@ -19,21 +15,17 @@ const Home = () => {
     useEffect(() => {
         const interval = setInterval(() => {
           setCurrentTime(new Date());
-        }, 1000); // Updates every second
+        }, 1000); 
     
         return () => clearInterval(interval);
     }, []);
+
+    const dueTasks = tasks?.filter(item=>item.dueDate === todayDate) || [];
 
     // Format date and time
     const formattedTime = currentTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit" });
     const formattedDate = currentTime.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
     const dayOfWeek = currentTime.toLocaleDateString("en-US", { weekday: "long" });
-
-    const getTotalNoOfTasks = () =>{
-        let sum = 0;
-        projects.forEach(project=>sum += project.tasks.length);
-        return sum;
-    }
 
     return ( 
         <div className={styles.home}>
@@ -50,12 +42,12 @@ const Home = () => {
             <div className={styles.pinned}>
                 <h4>Due Today</h4>
                 <div className={styles['pinned-tasks-container']}>
-                    {pinnedTasks && pinnedTasks.length > 0 ? pinnedTasks.map((task, index)=>(
-                        <div className={styles['home-task']}>
+                    {dueTasks && dueTasks.length > 0 ? dueTasks.map((task, index)=>(
+                        <div key={index} className={styles['home-task']}>
                             <p className={styles['task-title']}>{task.title}</p>
                             {task.dueDate ? <p className={styles['task-dueDate']}>{task.dueDate}</p> : null}
                         </div>  
-                    )) : null}
+                    )) : <p>No Due Tasks Today</p>}
                 </div>
             </div>
                 
