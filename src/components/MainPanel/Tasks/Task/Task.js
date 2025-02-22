29e2354ce7f34347e185a70a6e98cf86feb61f19
Stefan0712 <s.vladulescu@gmail.com>
2025-02-22 16:30:22 +0200

    Moved pinned icon to the left and made it smaller, changed the priority color to be a line at the end of the task title instead of a circle, chagned the border radius of checkboxes form 10px to 5px and changed selected task to have only side borders white

--- src/components/MainPanel/Tasks/Task/Task.js
+++ src/components/MainPanel/Tasks/Task/Task.js
@@ -20,7 +20,7 @@ const Task = ({data, isSelected, selectTask}) => {
         <div className={`${styles.task} ${isSelected && selectedTask === data.id ? styles.selected : ''}`} onClick={handleSelectTask}>
             <div className={styles.color} style={{backgroundColor: data.priority === 'high' ? 'red' : data.priority === 'normal' ? 'green' : data.priority === 'low' ? 'blue' : 'transparent'}}></div>
             <h4 style={data.isCompleted ? {textDecoration: 'line-through'} : null}>{data.title}</h4>
-            {data.isPinned ? <img className={`${styles['pin-task-icon']} small-icon`} src={IconLibrary.Pin} alt=''/> : null}
+            {data.isPinned ? <img className={`${styles['task-pin-icon']}`} src={IconLibrary.Pin} alt=''/> : null}
             {data.isCompleted ? <button onClick={handleCheckTask}><img className='small-icon' src={IconLibrary.Checkmark} /></button> : <input type='checkbox' onChange={handleCheckTask} checked={data.isCompleted}></input>}
         </div>
      );
