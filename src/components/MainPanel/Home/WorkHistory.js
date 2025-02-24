f506ab3d50063935963733c9ac53b97a63502843
Stefan0712 <s.vladulescu@gmail.com>
2025-02-24 18:12:04 +0200

    Replaced hardcoded values with dynamic ones inside the Work History

--- src/components/MainPanel/Home/WorkHistory.js
+++ src/components/MainPanel/Home/WorkHistory.js
@@ -1,10 +1,18 @@
+import { formatDate, formatTime } from '../../../helpers';
 import styles from './Home.module.css';
-
+import { useSelector } from 'react-redux';
 
 
 
 
 const WorkHistory = () => {
+    const date = new Date().toISOString().split("T")[0];
+    const history = useSelector(state=>state.appSettings.history)[date];
+
+    const focus = history.reduce((sum, item) => sum + item.focusSessions, 0);
+    const shortBreak = history.reduce((sum, item) => sum + item.breaks, 0);
+    const longBreak = history.reduce((sum, item) => sum + item.longBreaks, 0);
+    const totalTime = history.reduce((sum, item) => sum + item.totalTimeElapsed, 0);
     return ( 
         <div className={`${styles.module} ${'work-history'}`}>
             <div className={styles.header}>
@@ -13,19 +21,19 @@ const WorkHistory = () => {
             <div className={styles.content}>
                 <div className={styles['module-block']}>
                     <p className={styles['module-title']}>Focus</p>
-                    <p className={styles['module-value']}>3</p>
+                    <p className={styles['module-value']}>{focus}</p>
                 </div>
                 <div className={styles['module-block']}>
                     <p className={styles['module-title']}>Breaks</p>
-                    <p className={styles['module-value']}>6</p>
+                    <p className={styles['module-value']}>{shortBreak}</p>
                 </div>
                 <div className={styles['module-block']}>
                     <p className={styles['module-title']}>Long Breaks</p>
-                    <p className={styles['module-value']}>6</p>
+                    <p className={styles['module-value']}>{longBreak}</p>
                 </div>
                 <div className={styles['module-block']}>
                     <p className={styles['module-title']}>Total Time</p>
-                    <p className={styles['module-value']}>30 minutes</p>
+                    <p className={styles['module-value']}>{formatTime(totalTime)}</p>
                 </div>
             </div>
         </div>
